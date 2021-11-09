da3b43ff19b189e12dc596cf5b4b9bc061f7d00b
LuiCoro <luiacorona029@icloud.com>
2021-11-09 12:40:06 -0800

    '[WORKING] On Middleware!' - hopefully this makes the error portions of the test pass

--- api/accounts/accounts-middleware.js
+++ api/accounts/accounts-middleware.js
@@ -1,5 +1,16 @@
+const dbConfig = require('../../data/db-config')
+const Accounts = require('./accounts-model')
+
 exports.checkAccountPayload = (req, res, next) => {
   // DO YOUR MAGIC
+  try {
+    const {name , budget} = req.body
+
+
+  } catch (err) {
+
+  }
+
 }
 
 exports.checkAccountNameUnique = (req, res, next) => {
