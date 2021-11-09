b7b7608349b38b05f7df19c6fda6b58abf1db6e5
LuiCoro <luiacorona029@icloud.com>
2021-11-09 14:48:55 -0800

    '[WORKING] On Middleware!' - 2 out 3 Are working and linked [PASSED] 20 TEST [FAILED] 6 TEST

--- api/accounts/accounts-middleware.js
+++ api/accounts/accounts-middleware.js
@@ -4,17 +4,41 @@ const Accounts = require('./accounts-model')
 exports.checkAccountPayload = (req, res, next) => {
   // DO YOUR MAGIC
   try {
-    const {name , budget} = req.body
+    const {name, budget} = req.body
 
-
+    if (name === undefined || budget === undefined) {
+      res.status(400).json({message: 'name and budget are required'})
+    } else if (typeof name !== 'string') {
+      res.status(400).json({message: 'name of account must be a string'})
+    } else if (name.trim().length < 3 || name.trim().length > 100) {
+      res.status(400).json({message: 'name of account must be between 3 and 100'})
+    } else if (typeof budget !== 'number') {
+      res.status(400).json({message: 'budget of account must be a number'})
+    } else if (req.body.budget < 0 || req.body.budget > 1000000) {
+      res.status(400).json({message: 'budget of account is too large or too small'})
+    } else {
+      next()
+    }
   } catch (err) {
-
+    res.status(201).json()
+    next(err)
   }
-
 }
 
-exports.checkAccountNameUnique = (req, res, next) => {
+exports.checkAccountNameUnique = async (req, res, next) => {
   // DO YOUR MAGIC
+  try {
+    const data = await dbConfig('accounts').where('name', req.body.name.trim()).first()
+
+    if (data) {
+      res.status(400).json({message: 'that name is already taken'})
+    } else {
+      next()
+    }
+
+  } catch (err) {
+    next(err)
+  }
 }
 
 exports.checkAccountId = (req, res, next) => {
--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -1,18 +1,19 @@
 const router = require('express').Router()
 const Accounts = require('./accounts-model')
 
-router.get('/', async (req, res) => {
+const {
+  checkAccountPayload,
+  checkAccountNameUnique
+} = require('./accounts-middleware')
+
+router.get('/', async (req, res, next) => {
   // DO YOUR MAGIC
   try {
     const data = await Accounts.getAll()
-    if (!data) {
-      res.status(400).json({message: 'Can not get all accounts!'})
-    } else {
-      res.status(200).json(data)
-    }
 
+    res.status(200).json(data)
   } catch (err) {
-    res.status(500).json({message: 'Error with the server check code!'})
+    next(err)
   }
 })
 
@@ -33,20 +34,29 @@ router.get('/:id', async (req, res) => {
   }
 })
 
-router.post('/', async (req, res) => {
+
+// try {
+//   const {name, budget} = req.body
+//
+//   if (!name || !budget) {
+//     res.status(400).json({message: 'name and budget are undefined'})
+//   } else {
+//     const newAccount = await Accounts.create({name, budget})
+//     res.status(201).json(newAccount)
+//   }
+//
+// } catch (err) {
+//   res.status(500).json({message: 'Error with the server check code!'})
+// }
+
+
+router.post('/', checkAccountPayload, checkAccountNameUnique, async (req, res, next) => {
   // DO YOUR MAGIC
   try {
-    const {name, budget} = req.body
-
-    if (!name || !budget) {
-      res.status(400).json({message: 'name and budget are undefined'})
-    } else {
-      const newAccount = await Accounts.create({name, budget})
-      res.status(201).json(newAccount)
-    }
-
+    const newAccount = await Accounts.create(req.body)
+    res.status(201).json(newAccount)
   } catch (err) {
-    res.status(500).json({message: 'Error with the server check code!'})
+    next(err)
   }
 })
 
@@ -80,13 +90,13 @@ router.delete('/:id', async (req, res) => {
     const {id} = req.params
     const deletedAccount = await Accounts.deleteById(id)
 
-    if(!deletedAccount) {
+    if (!deletedAccount) {
       res.status(404).json({message: 'account not found'})
     } else {
       res.status(200).json(deletedAccount)
     }
   } catch (err) {
-    res.status(500).json({message : 'Error with the server check code!'})
+    res.status(500).json({message: 'Error with the server check code!'})
   }
 
 })
